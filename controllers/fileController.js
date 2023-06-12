5db0e27f7816595041113ee142239f4850d6577b
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-06-12 20:22:19 +0530

    added error handler

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,35 +1,39 @@
 const ipfs = require("../ipfsServer");
 const mime = require('mime-types');
+const CatchAsyncError = require("../middleware/catchAsynError")
 
-exports.uploadFile = async (req, res, next) => {
-
-    // console.log(req.file)
-    const { cid } = await ipfs.add(req.file.buffer);
-    console.log(`${cid}`)
+exports.uploadFile = CatchAsyncError( async (req, res, next) => {
+    console.log(req.file)
+    const {cid} = await ipfs.add(req.file.buffer);
     const url = `https://gateway.ipfs.io/ipfs/${cid}`;
     console.log(url)
 
     res.status(200).json({
         success: true,
-        cid,
+        cid: cid.toString(),
         url
     })
-};
+})
 
 
 exports.getFile = async (req, res, next) => {
+
     const cid = req.params.cid;
     const chunks = [];
+
     for await (const chunk of ipfs.cat(cid)) {
         chunks.push(chunk);
     }
-    const buffer = Buffer.concat(chunks);
-    const file = buffer.toString();
-    const mimeType = mime.lookup(file);
-    console.log(mimeType)
+
+    const buffer = Buffer.concat(chunks); // convert array of buffers into single buffer
+    const file = buffer.toString(); // convert buffer to string
+    const mimeType = mime.lookup(file); // get file type
+    // console.log(mimeType)
+
     res.writeHead(200, {
         'Content-Type': mimeType,
         'Content-Disposition': `attachment; filename=${cid}`
     });
+
     res.end(buffer);
 }
