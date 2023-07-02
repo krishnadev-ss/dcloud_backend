84457d0a2bf8cd4bbed8f650f77198df22a52579
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-03 03:10:32 +0530

    feat: configured db

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+
+const connectDB = () => {
+    mongoose.connect(process.env.DB_URI)
+        .then((data) => {
+            console.log("Connected to database with server " + data.connection.host);
+        })
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const app = require("./app")
+const app = require("./app");
+const connectDB = require("./config/database");
+const dotenv = require("dotenv");
 
+dotenv.config({path: "config/config.env"})
+connectDB();
 
-const PORT = 8000;
-const server = app.listen(PORT, (req, res) => {
-    console.log(`starting server on ${PORT}`)
-})
+const server = app.listen(process.env.PORT, (req, res) => {
+    console.log(`starting server on ${process.env.PORT}`)
+});
