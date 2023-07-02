e6a630a43c7b6dd6632027b0391182e5fbed7131
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-03 04:00:21 +0530

    feat: uncaughtException handler

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const app = require("./app");
 const connectDB = require("./config/database");
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", err => {
+    console.log("Error: " + err.message);
+    console.log("Shutting down the server due to uncaughtException");
+    process.exit(1);
+})
+
+
 dotenv.config({path: "config/config.env"})
 connectDB();
+
+
 
 const server = app.listen(process.env.PORT, (req, res) => {
     console.log(`starting server on ${process.env.PORT}`)
