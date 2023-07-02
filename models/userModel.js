ae160e062761b89d6fe3a2271cb6ed915307e291
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-03 03:10:20 +0530

    feat: created user model

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "Please enter a name"],
+        maxLength: [20, "Your name cannot exceed 20 characters"],
+        minLength: [3, "Your name cannot be less than 4 characters"]
+    },
+    email: {
+        type: String,
+        require: [true, "Please enter a email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter a valid email"]
+    },
+    password: {
+        type: String,
+        require: [true, "Please enter a password"],
+        minLength: [8, 'Your password must be longer than 8 characters'],
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password"))
+        next();
+    return await bcrypt.hash(this.password, 10);
+})
+
+userSchema.methods.getJWTToken = function () {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRETE, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+}
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.comparePassword(enteredPassword, this.password);
+}
+
+module.exports = mongoose.model("User", userSchema);
