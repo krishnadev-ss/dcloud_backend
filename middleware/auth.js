07cc1472bc71a8f4c9611899e3823b2ae2c46194
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-03 05:53:41 +0530

    fix: auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,17 +5,24 @@ const User = require("../models/userModel")
 
 exports.isAuthenticatedUser = CatchAsyncError(async (req, res, next) => {
 
-    if(!req.headers.authorization)
-        return next(new ErrorHandler("no token found", 401));
+    if(req.cookies.token) {
+        if(!req.headers.authorization)
+            return next(new ErrorHandler("no token found", 401));
 
-    const token = req.headers.authorization.split(" ")[1]
+        const token = req.headers.authorization.split(" ")[1]
 
-    if(!token || token === "j:null")
+        if(!token || token === "j:null")
+            return next(new ErrorHandler("please login to access this resource", 401));
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRETE);
+
+        const user = await User.findById(decodedData.id);
+        req.user = user
+
+        next();
+    }
+    else {
         return next(new ErrorHandler("please login to access this resource", 401));
+    }
 
-    const decodedData = jwt.verify(token, process.env.JWT_SECRETE);
-
-    req.user = await User.findById(decodedData.id);
-
-    next();
 });
