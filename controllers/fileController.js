1e3b3eac4a0bfc2a2a69a46f25edb7f3093ce757
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-11 21:41:05 +0530

    added search feature in get files route

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -4,23 +4,23 @@ const CatchAsyncError = require("../middleware/catchAsyncError");
 const File = require("../models/fileModel");
 const ErrorHandler = require("../utils/errorHandler");
 
-exports.uploadFile = CatchAsyncError( async (req, res, next) => {
+exports.uploadFile = CatchAsyncError(async (req, res, next) => {
 
-    if(!req.file)
+    if (!req.file)
         return next(new ErrorHandler("Please upload a file", 400));
 
     let addResult = await ipfs.add(req.file.buffer);
     const {cid} = addResult;
     const url = `https://gateway.ipfs.io/ipfs/${cid}`;
     let type;
-    if(req.file.mimetype === "application/pdf" || req.file.mimetype === "application/msword" || req.file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-        type = "Document"
-    else if(req.file.mimetype === "image/jpeg" || req.file.mimetype === "image/png" || req.file.mimetype === "image/gif" || req.file.mimetype === "image/tiff" || req.file.mimetype === "image/bmp" || req.file.mimetype === "image/webp" || req.file.mimetype === "image/vnd.microsoft.icon" || req.file.mimetype === "image/svg+xml" || req.file.mimetype === "image/x-icon" || req.file.mimetype === "image/vnd.djvu" || req.file.mimetype === "image/avif" || req.file.mimetype === "image/apng" || req.file.mimetype === "image/flif" || req.file.mimetype === "image/x-portable-pixmap" || req.file.mimetype === "image/x-portable-anymap" || req.file.mimetype === "image/x-portable-bitmap" || req.file.mimetype === "image/x-portable-graymap" || req.file.mimetype === "image/x-portable-arbitrarymap" || req.file.mimetype === "image/x-portable-bitmap" || req.file.mimetype === "image/x-portable-pixmap" || req.file.mimetype === "image/x-portable-anymap" || req.file.mimetype === "image/x-portable-graymap" || req.file.mimetype === "image/x-portable-arbitrarymap")
-        type = "Image"
-    else if(req.file.mimetype === "video/mp4" || req.file.mimetype === "video/mpeg" || req.file.mimetype === "video/ogg" || req.file.mimetype === "video/quicktime" || req.file.mimetype === "video/webm" || req.file.mimetype === "video/x-ms-wmv" || req.file.mimetype === "video/x-flv" || req.file.mimetype === "video/x-msvideo" || req.file.mimetype === "video/3gpp" || req.file.mimetype === "video/3gpp2" || req.file.mimetype === "video/x-matroska" || req.file.mimetype === "video/x-m4v" || req.file.mimetype === "video/avi" || req.file.mimetype === "video/x-ms-asf" || req.file.mimetype === "video/x-mpegURL" || req.file.mimetype === "video/MP2T" || req.file.mimetype === "video/x-msvideo" || req.file.mimetype === "video/x-flv" || req.file.mimetype === "video/x-ms-wmv" || req.file.mimetype === "video/x-ms-asf" || req.file.mimetype === "video/x-mpegURL" || req.file.mimetype === "video/MP2T" || req.file.mimetype === "video/x-m4v" || req.file.mimetype === "video/avi" || req.file.mimetype === "video/x-matroska")
-        type = "Video"
+    if (req.file.mimetype === "application/pdf" || req.file.mimetype === "application/msword" || req.file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+        type = "document"
+    else if (req.file.mimetype === "image/jpeg" || req.file.mimetype === "image/png" || req.file.mimetype === "image/gif" || req.file.mimetype === "image/tiff" || req.file.mimetype === "image/bmp" || req.file.mimetype === "image/webp" || req.file.mimetype === "image/vnd.microsoft.icon" || req.file.mimetype === "image/svg+xml" || req.file.mimetype === "image/x-icon" || req.file.mimetype === "image/vnd.djvu" || req.file.mimetype === "image/avif" || req.file.mimetype === "image/apng" || req.file.mimetype === "image/flif" || req.file.mimetype === "image/x-portable-pixmap" || req.file.mimetype === "image/x-portable-anymap" || req.file.mimetype === "image/x-portable-bitmap" || req.file.mimetype === "image/x-portable-graymap" || req.file.mimetype === "image/x-portable-arbitrarymap" || req.file.mimetype === "image/x-portable-bitmap" || req.file.mimetype === "image/x-portable-pixmap" || req.file.mimetype === "image/x-portable-anymap" || req.file.mimetype === "image/x-portable-graymap" || req.file.mimetype === "image/x-portable-arbitrarymap")
+        type = "image"
+    else if (req.file.mimetype === "video/mp4" || req.file.mimetype === "video/mpeg" || req.file.mimetype === "video/ogg" || req.file.mimetype === "video/quicktime" || req.file.mimetype === "video/webm" || req.file.mimetype === "video/x-ms-wmv" || req.file.mimetype === "video/x-flv" || req.file.mimetype === "video/x-msvideo" || req.file.mimetype === "video/3gpp" || req.file.mimetype === "video/3gpp2" || req.file.mimetype === "video/x-matroska" || req.file.mimetype === "video/x-m4v" || req.file.mimetype === "video/avi" || req.file.mimetype === "video/x-ms-asf" || req.file.mimetype === "video/x-mpegURL" || req.file.mimetype === "video/MP2T" || req.file.mimetype === "video/x-msvideo" || req.file.mimetype === "video/x-flv" || req.file.mimetype === "video/x-ms-wmv" || req.file.mimetype === "video/x-ms-asf" || req.file.mimetype === "video/x-mpegURL" || req.file.mimetype === "video/MP2T" || req.file.mimetype === "video/x-m4v" || req.file.mimetype === "video/avi" || req.file.mimetype === "video/x-matroska")
+        type = "video"
     else
-        type = "Other"
+        type = "other"
 
     const file = await File.create({
         name: req.file.originalname,
@@ -30,8 +30,6 @@ exports.uploadFile = CatchAsyncError( async (req, res, next) => {
         type,
         size: req.file.size
     })
-
-
 
     res.status(200).json({
         success: true,
@@ -46,16 +44,15 @@ exports.getFile = async (req, res, next) => {
     console.log(id)
     const owner = await File.findById(id).select("owner");
 
-    if(!owner)
+    if (!owner)
         return next(new ErrorHandler("File not found", 404));
 
     const sharedWith = await File.findOne({id}).select("sharedWith");
 
 
-
-    if(!owner.owner.toString() === req.user._id.toString()) {
-        if(sharedWith) {
-            if(!sharedWith.sharedWith.includes(req.user._id.toString())) {
+    if (!owner.owner.toString() === req.user._id.toString()) {
+        if (sharedWith) {
+            if (!sharedWith.sharedWith.includes(req.user._id.toString())) {
                 return next(new ErrorHandler("You are not authorized to access this file", 401))
             }
 
@@ -76,14 +73,14 @@ exports.downloadFile = async (req, res, next) => {
     console.log(id)
     const owner = await File.findById(id).select("owner");
 
-    if(!owner)
+    if (!owner)
         return next(new ErrorHandler("File not found", 404));
 
     const sharedWith = await File.findOne({id}).select("sharedWith");
 
-    if(!owner.owner.toString() === req.user._id.toString()) {
-        if(sharedWith) {
-            if(!sharedWith.sharedWith.includes(req.user._id.toString())) {
+    if (!owner.owner.toString() === req.user._id.toString()) {
+        if (sharedWith) {
+            if (!sharedWith.sharedWith.includes(req.user._id.toString())) {
                 return next(new ErrorHandler("You are not authorized to access this file", 401))
             }
 
@@ -114,9 +111,27 @@ exports.downloadFile = async (req, res, next) => {
 
 
 exports.getFiles = CatchAsyncError(async (req, res, next) => {
-    const files = await File.find({owner: req.user._id});
+
+    const condition = {}
+
+    if (req.query.type)
+        condition.type = req.query.type;
+
+    if (req.query.shared)
+        condition.sharedWith = req.user._id;
+
+    if (req.query.keyword) {
+        condition.$or = [
+            {name: { $regex: req.query.keyword, $options: 'i' }},
+            {type: {$regex: req.query.keyword,$options: 'i'}}
+        ]
+    }
+
+    const files = await File.find(condition);
+
     res.status(200).json({
         success: true,
+        count: files.length,
         files
     })
 })
