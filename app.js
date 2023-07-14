469f0c97605b7ea11b09f02bd9aaf499aca452ad
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-15 02:28:54 +0530

    added express file uploader

--- app.js
+++ app.js
@@ -4,16 +4,69 @@ const bodyParser = require("body-parser");
 const cookieParse = require("cookie-parser");
 const errorMiddleWare = require("./middleware/error");
 const cors = require("cors");
-
+const fileUpload = require("express-fileupload")
 
 const app = express();
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors());
+app.use(express.json()); // This is a middleware that allows us to accept json data in the body
 app.use(cookieParse());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload());
+
+
+// ANSI escape sequences for colors and styles
+const RESET = '\x1b[0m';
+const LIGHT_GREEN = '\x1b[92m';
+const YELLOW = '\x1b[33m';
+const LIGHT_BLUE = '\x1b[94m';
+const RED = '\x1b[31m';
+const GRAYISH = '\x1b[37m';
+
+// Middleware to log incoming requests with colored method and URL
+app.use((req, res, next) => {
+    const arrow = `${GRAYISH}==> `;
+    const timestamp = new Date().toISOString();
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const method = req.method;
+    const url = req.url;
+
+    let methodColor;
+    switch (method) {
+        case 'GET':
+            methodColor = LIGHT_GREEN;
+            break;
+        case 'POST':
+            methodColor = YELLOW;
+            break;
+        case 'DELETE':
+            methodColor = RED;
+            break;
+        case 'PUT':
+            methodColor = LIGHT_BLUE;
+            break;
+        default:
+            methodColor = RESET;
+    }
+
+    console.log(`\n${arrow}[${timestamp}] Incoming request: ${methodColor}${method}${RESET} ${GRAYISH}${url}${RESET} from ${ip}`);
+    // console.log(`${GRAYISH}Headers:${RESET}`, req.headers);
+    // console.log(`${GRAYISH}Body:${RESET}`, req.body);
+
+    res.on('finish', () => {
+        const statusCode = res.statusCode;
+        let statusColor;
+        if (statusCode >= 300) {
+            statusColor = RED;
+        } else {
+            statusColor = LIGHT_GREEN;
+        }
+        console.log(`${GRAYISH}[${timestamp}] Response status: ${statusColor}${statusCode}${RESET}`);
+    });
+
+    next();
+});
+
 
 const fileRouter = require("./routes/fileRoute");
 const userRouter = require("./routes/userRoute");
@@ -22,6 +75,7 @@ app.use("/api", fileRouter)
 app.use("/api/auth", userRouter)
 
 app.use(errorMiddleWare);
+
 
 
 // const url = "http://localhost:7545";
