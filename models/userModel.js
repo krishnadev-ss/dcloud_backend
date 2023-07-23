e5f4d07a599f71d1b66fb8aabbdc27d301b0ed6f
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-23 23:16:45 +0530

    fix: bcrypt hashing is not happenig

--- models/userModel.js
+++ models/userModel.js
@@ -40,7 +40,9 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password"))
         return next();
-    return await bcrypt.hash(this.password, 10);
+
+    this.password = await bcrypt.hash(this.password, 10);
+    return this.password;
 });
 
 userSchema.methods.comparePassword = async function(enteredPassword) {
