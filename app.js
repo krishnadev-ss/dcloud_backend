ae7e2b24cdef84e66c1695d5dc40ba15264fc143
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-08-14 04:49:04 +0530

    added production origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3000"],
+    origin: "https://dcloud-nayanprasad.vercel.app",
     credentials: true,
 }));
 app.use(express.json());
