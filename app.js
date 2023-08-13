a8b6debd2c23e0d08047446b27d8e2d745b350f9
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-08-14 04:38:34 +0530

    added production origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ["http://localhost:3000", "http://localhost:3000"],
     credentials: true,
 }));
 app.use(express.json());
